b7a85c5d423e8c75da13f5864e5c9ee0731d1d00
Mouatassim Boulahsen <boulahsen.mouatassim@gmail.com>
2022-01-20 17:02:32 -0500

    Added todoItems collection class

---
+++ src/todoCollection.ts
@@ -0,0 +1,47 @@
+import TodoItem from "./todoItem";
+
+export default class TodoCollection {
+  private _nextId: number = 1;
+  private _itemMap: Map<number, TodoItem>;
+  public userName: string;
+
+  constructor(userName: string, todoItems: TodoItem[] = []) {
+    this.userName = userName;
+    this._itemMap = new Map<number, TodoItem>();
+    todoItems.forEach((item) => this._itemMap.set(item.id, item));
+  }
+
+  public addTodo(task: string): number {
+    while (this.getTodoById(this._nextId)) {
+      this._nextId++;
+    }
+
+    this._itemMap.set(this._nextId, new TodoItem(this._nextId, task));
+    return this._nextId;
+  }
+
+  public getTodoById(id: number): TodoItem | undefined {
+    return this._itemMap.get(id);
+  }
+
+  public getTodoItems(includeComeplete: boolean): TodoItem[] {
+    return [...this._itemMap.values()].filter(
+      (item) => includeComeplete || !item.complete
+    );
+  }
+
+  public markComplete(id: number, complete: boolean): void {
+    const todoItem = this.getTodoById(id);
+    if (todoItem) {
+      todoItem.complete = complete;
+    }
+  }
+
+  public removeComplete(): void {
+    this._itemMap.forEach((item) => {
+      if (item.complete) {
+        this._itemMap.delete(item.id);
+      }
+    });
+  }
+}
