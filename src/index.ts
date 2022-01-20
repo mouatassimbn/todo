4a207364b60a023320003e31eca6133061ee2c0b
Mouatassim Boulahsen <boulahsen.mouatassim@gmail.com>
2022-01-20 17:03:21 -0500

    Implemted todo collection

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import TodoItem from "./todoItem";
+import TodoCollection from "./todoCollection";
+
+let todos: TodoItem[] = [
+  new TodoItem(1, "By Flowers"),
+  new TodoItem(2, "Get Shoews"),
+  new TodoItem(3, "Collect Tickets"),
+  new TodoItem(4, "Call Joe", true),
+];
+
+let collection = new TodoCollection("Adam", todos);
+
+console.clear();
+console.log(`${collection.userName}'s Todo List`);
+collection.removeComplete();
+collection.getTodoItems(true).forEach((item) => item.printDetails());
