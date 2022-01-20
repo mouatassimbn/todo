2903d38ee03de4424579faa55b9fe543c652d7f3
Mouatassim Boulahsen <boulahsen.mouatassim@gmail.com>
2022-01-20 17:08:50 -0500

    implemnted logic in client

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,8 @@ let todos: TodoItem[] = [
 let collection = new TodoCollection("Adam", todos);
 
 console.clear();
-console.log(`${collection.userName}'s Todo List`);
-collection.removeComplete();
+console.log(
+  `${collection.userName}'s Todo List`,
+  `(${collection.getItemCounts().incomplete} items to do)`
+);
 collection.getTodoItems(true).forEach((item) => item.printDetails());
